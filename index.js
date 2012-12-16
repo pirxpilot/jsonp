9426648886b98cea112a6de37ab16a7b2c28bdd6
Damian Krzeminski <dkrzemin@gmail.com>
2012-12-15 18:08:44 -0800

    Basic version of API
    Support for query and end parameters

---
+++ index.js
@@ -0,0 +1,59 @@
+// unique callback number
+var callbackId = 0;
+
+function getCallbackName() {
+  callbackId += 1;
+  return 'jsonp' + callbackId;
+}
+
+function prepareUrl(url, params) {
+  return url + '?' + Object.keys(params)
+    .map(function(key) {
+      return key + '=' + encodeURIComponent(params[key]);
+    })
+    .join('&');
+}
+
+module.exports = function jsonp(url, fn) {
+  var self, my = {
+    url: url,
+    query: {}
+  };
+
+  function query(q) {
+    Object.keys(q).forEach(function(key) {
+      my.query[key] = q[key];
+    });
+    return self;
+  }
+
+  function end(fn) {
+    var js, fjs, fnName = getCallbackName();
+
+    window[fnName] = function(json) {
+      // cleanup after the call
+      delete window[fnName];
+      js.parentNode.removeChild(js);
+      // execute provided callback
+      fn(json);
+    };
+
+    my.query.callback = fnName;
+    js = document.createElement('script');
+    js.src = prepareUrl(my.url, my.query);
+    js.async = true;
+    fjs = document.getElementsByTagName('script')[0];
+    fjs.parentNode.insertBefore(js, fjs);
+  }
+
+  if (typeof fn === 'function') {
+    return end(fn);
+  }
+
+  self = {
+    query: query,
+    end: end
+  };
+
+  return self;
+};
